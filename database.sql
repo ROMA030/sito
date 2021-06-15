991233d91aafb8e4014c3403be00837a70221257
FrancescoTonassi <57535330+FrancescoTonassi@users.noreply.github.com>
2021-06-15 12:16:56 +0200

    added field

--- database.sql
+++ database.sql
@@ -1,4 +1,7 @@
 CREATE TABLE `users` (
     `Utente` varchar(255),
-    `Password` varchar(40)
+    `Password` varchar(40),
+    `Nome` varchar(40),
+    `Cognome` varchar(40),
+    `DataNascita` varchar(255)
 )

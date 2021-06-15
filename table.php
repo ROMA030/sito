88bba4948dfd1c5a1ca9338bf1fc75d9f5f05a74
FrancescoTonassi <57535330+FrancescoTonassi@users.noreply.github.com>
2021-06-15 17:00:21 +0200

    added search (don't work completely)

--- table.php
+++ table.php
@@ -8,16 +8,20 @@
         <link rel="stylesheet" href="style.css">
         </link>
 
-        
     </head>
 
     <body>
         <header class="header clearfix">
-                <a class="header__logo">Logo</a>
-                <a href=""></a>
+            <a class="header__logo">Logo</a>
         </header>
 
         <div>
+
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="GET">
+                <input name ="user" type="text" placeholder="Username">
+                <input name="submit" type="submit" value="Search">
+            </form>
+
             <?php
                 $name = $surname = $birthday = $username = $password = "";
                 // Used for connect to the database called "sito"
@@ -50,15 +54,46 @@
                         echo "<td>". $row['Utente']. "</td>";
                         echo "<td>". $row['Password']. "</td>";
                         echo "<td>". $row['DataNascita']. "</td>";
-                        echo "<td><a id='table__hype' href=\"delete.php?user=".$row['Utente']."\">Delete</a></td>";
+                        echo "<td><a href=\"delete.php?user=".$row['Utente']."\">Delete</a></td>";
                         echo "</tr>";
                     }
                 }
                 echo "</table>";
+                
+                if ($_SERVER["REQUEST_METHOD"] == "GET") {
+                    $username = $_GET["user"];
+
+                    $search = "SELECT * FROM users WHERE Utente = '$username'";        
+                    $resultSearch = mysqli_query($conn, $search);
+
+                    echo "<table style='width:100%' id='t01'>"; 
+                    echo "<tr>";
+                    echo "<th>Name</th>";
+                    echo "<th>Surname</th>";
+                    echo "<th>Username</th> ";
+                    echo "<th>Password</th>";
+                    echo "<th>Birthday</th>";
+                    echo "<th>Actions</th>";
+                    echo "</tr>";
+                 
+                    if (mysqli_num_rows($result) > 0) {
+                        while($row = $resultSearch->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>". $row['Nome']. "</td>";
+                            echo "<td>". $row['Cognome']. "</td>";
+                            echo "<td>". $row['Utente']. "</td>";
+                            echo "<td>". $row['Password']. "</td>";
+                            echo "<td>". $row['DataNascita']. "</td>";
+                            echo "<td><a href=\"delete.php?user=".$row['Utente']."\">Delete</a></td>";
+                            echo "</tr>";
+                        }
+
+                    }
+
+                }
                 mysqli_close($conn);
             ?>
+
         </div>
-        
-        <input type="text">
     </body>
 </html>

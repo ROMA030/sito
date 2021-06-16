c714e7dbc88651ed4c492bbc844c13262f4eabe2
ROMA030 <dani21roby@gmail.com>
2021-06-16 13:28:31 +0200

    Back register fixed

--- table.php
+++ table.php
@@ -3,143 +3,19 @@
 <html lang="en">
 
     <head>
+
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-        <link rel="stylesheet" href="style.css">
-        </link>
+        <link rel="stylesheet" href="style.css"></link>
+    </link>
 
     </head>
 
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-            font-family: "Arial";
-        }
-
-
-        .wrapper .header {
-            background: #29a5d8;
-        }
-
-        .wrapper .title {
-            padding: 50px 0;
-            text-align: center;
-            font-weight: 700;
-            font-size: 32px;
-            color: #fff;
-        }
-
-        .wrapper .search_box {
-            max-width: 1000px;
-            background: #29a5d8;
-            margin: 0 auto;
-            padding: 25px 0 50px;
-            border-radius: 3px;
-        }
-
-        .wrapper .search_box input {
-            border: 0;
-            border-bottom: 2px solid #e5edfa;
-            width: 100%;
-            outline: none;
-            padding: 10px;
-            background: transparent;
-            color: #fff;
-            font-size: 16px;
-        }
-
-        ::placeholder {
-            color: #121212;
-        }
-
-        .wrapper .search_box input:focus {
-            border-bottom: 2px solid #fff;
-        }
-
-        .table_wrap {
-            width: 1500px;
-            margin: 50px auto 0;
-        }
-
-        .table_wrap ul li .item {
-            display: flex;
-            align-items: center;
-            background: #fff;
-            padding: 15px 0;
-            height: 50px;
-        }
-
-        .table_wrap ul li .item .name,
-        .table_wrap ul li .item .surname,
-        .table_wrap ul li .item .password,
-        .table_wrap ul li .item .actions,
-        .table_wrap ul li .item .birthday,
-        .table_wrap ul li .item .username {
-            width: 20%;
-            padding: 0 15px;
-        }
-
-        .table_wrap ul li .item .name span {
-            width: 90%;
-            display: inline-block;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-        }
-
-        .table_header ul li .item {
-            border-bottom: 2px solid #eaeaea;
-            font-weight: 600;
-        }
-
-        .table_body {
-            height: 300px;
-            overflow: auto;
-        }
-
-        .table_body ul li .item {
-            border-bottom: 1px solid #efefef;
-        }
-
-        .table_body ul li .item .open {
-            background: #e5edfa;
-            color: #5a8ee4;
-        }
-
-        .table_body ul li .item .fixed {
-            background: #cff1f0;
-            color: #0dbeb6;
-        }
-
-        .table_body ul li .item .closed {
-            background: #fedfe5;
-            color: #ff89a0;
-        }
-
-        .table_body ul li .item .hold {
-            background: #fff0db;
-            color: #f59d39;
-        }
-
-        .table_body ul li .item .reopened {
-            background: #d6f2ff;
-            color: #29a5d8;
-        }
-
-        .table_body ul li .item .canceled {
-            background: #ffdbd6;
-            color: #e87060;
-        }
-    </style>
 
     <body>
         <header class="header clearfix">
                 <a class="header__logo">Logo</a>
-                <a href=""></a>
-                <button  type="button" class="header__back" ><a href="index.php">Back</a></button>
+                <a href="index.php" class="header__back1">Back</a>
         </header>
 
         <div>
